341d8eb034bd1270bad6232ce2ba09775b76a74a
github username <github email address>
2018-07-24 18:05:33 -0400

    OO- School Domain Lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_reader :name, :roster, :student
+
+
+  def initialize(name)
+    @name = name
+    @student = student
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, students|
+      roster[grade] = students.sort
+    end
+    roster
+  end
+end
